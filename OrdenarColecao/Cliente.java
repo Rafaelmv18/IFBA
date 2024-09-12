6a5030133d54b08495a11435d7c43be33ff36362
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2024-09-12 09:57:05 -0300

    LpII

--- OrdenarColecao/Cliente.java
+++ OrdenarColecao/Cliente.java
@@ -6,15 +6,28 @@ public class Cliente{
     public static void main(String[] args){
 
         List<Conta> contas = new ArrayList<Conta>();
-        Conta conta1 = new Conta(1, "Rafael", 1000.0f, 500.0f);
+        Conta conta1 = new Conta();
+
+        conta1.setNumero(1);
+        conta1.setTitular("Rafael");
+        conta1.setSaldo(1000.0f);
+        conta1.setLimiteCredito(500.0f);
         contas.add(conta1);
 
-        Conta conta2 = new Conta(2, "Lucas", 8009.0f, 800.0f);
+        Conta conta2 = new Conta();
+        conta2.setNumero(2);
+        conta2.setTitular("Lucas");
+        conta2.setSaldo(8009.0f);
+        conta2.setLimiteCredito(800.0f);
         contas.add(conta2);
 
-        Conta conta3 = new Conta(3, "Matheus", 97804.0f, 2000.0f);
+        Conta conta3 = new Conta();
+        conta3.setNumero(3);
+        conta3.setTitular("Mathes");
+        conta3.setSaldo(97804.0f);
+        conta3.setLimiteCredito(2000.0f);
         contas.add(conta3);
-       
+
        Collections.sort(contas);
 
        for(Conta conta : contas){
